a21d3cbebd2ea54009b8c781ffb5d9bca95eaa3f
AdomasDauda <107749872+AdomasDauda@users.noreply.github.com>
2022-06-28 12:30:47 +0100

    bubblesort + digital/analog scale
    some crazy version but looks interesting
    (added bubblesort)

--- display.py
+++ display.py
@@ -24,7 +24,8 @@ def selectionSortForTest(array):
 
 # shuffle the numbers in the array
 # (i didnt want to used random.shuffle())
-def shuffler (arr, n):
+def shuffler (arr):
+    n = len(arr)
     # We will Start from the last element
     # and swap one by one.
     for i in range(n-1):
@@ -59,6 +60,21 @@ def sum(array):
     return(sum)
 
 
+def flipbool(bool):
+    if bool is not True:
+        bool = True
+    else:
+        bool = False
+    return bool
+
+#generate new array in the same array
+def generateNewArray():
+    generatedArray = ([])
+    generatedArray = np.random.randint(1, 200, size = getCurrentValue()+5)
+    print(generatedArray, "generator array")
+    return generatedArray
+
+
 # find minimum value of the array
 def minim(array):
     min = len(array)
@@ -79,19 +95,30 @@ def findIndexByNumber(array, number):
         return(NumberIndex)
 
 
-def drawingGraph(arr,canvas,rectLastX, width):
+def drawingGraph(array,canvas):
+    width = 50 #width of the collumn
+    rectLastX = 50 # distance from left top edge
+    print(array, "drawing graph array")
     canvas.delete("all")
     # for every number in array make a rectangle the height of the number in the array * 10
-    for i in range(arr.size):
-        case = arr[i]
+    for i in range(array.size):
+        case = array[i]
         #if case !=0: # zeroes will have a height of 0 so i dont want to display them
         canvas.create_rectangle(rectLastX+width, 0, rectLastX+10, case*4,fill='green')
         rectLastX += width
         canvas.create_text(rectLastX-width/2, case*4,text=case, fill="Black")
         canvas.pack()
 
+
+def getCurrentValue():
+    return round(currentValue.get())
+
+
+def sliderChanged(event):
+    print(getCurrentValue())
     
 def selectionSort(array,index):
+    print(array, "selection sort array")
     #print(array, "selection sorter")
     minNumber = array[index]
     minIndex = index
@@ -102,46 +129,71 @@ def selectionSort(array,index):
     array[minIndex],array[index] = array[index],array[minIndex]
     return array
 
-def animateSelectionSort(window,canvas,array,sortedArray):
-    while not np.array_equal(array,sortedArray):
-        for i in range(array.size):
-            array = selectionSort(array, i)
-            drawingGraph(array,canvas,rectLastX,width)
-            canvas.create_line(2*width+width*i, 0, 2*width+i*width, len(array)*15, width=4) #<---- I want to implement a barrier between sorted and unsorted numbers
-            canvas.create_text(2*width+width*i, len(array)*15,text="After this line everything is sorted",fill="black")
-            window.update()
-            time.sleep(animation_refresh_seconds)
+
+def bubbleSort(array):
+        for i in range(array.size-1):
+            if array[i] > array[i+1]:
+                array[i],array[i+1] = array[i+1],array[i]
+        return array
+
+
+def animateBubbleSort(window,canvas, array):
+    for j in range(array.size):
+        arraycopy = copy(array)
+        array = bubbleSort(array)
+        drawingGraph(array,canvas)
+        if np.array_equal(array, arraycopy):
+            break
+        window.update()
+        time.sleep(animation_refresh_seconds)
+
+
+def animateSelectionSort(window,canvas,array):
+    width = 50 #width of the collumn
+    rectLastX = 50 # distance from left top edge
+    for i in range(array.size):
+        array = selectionSort(array, i)
+        drawingGraph(array,canvas)
+        canvas.create_line(2*width+width*i, 0, 2*width+i*width, len(array)*15, width=4) #<---- I want to implement a barrier between sorted and unsorted numbers
+        canvas.create_text(2*width+width*i, len(array)*15,text="After this line everything is sorted",fill="black")
+        window.update()
+        time.sleep(animation_refresh_seconds)
             
 #***main code***#
 
-def animateSorting(window,canvas,array,sortedArray):
-    #print(sortedArray)
-    #print(array)
-    drawingGraph(array,canvas,rectLastX,width)
-    selectionSort = tkinter.Button(window, text="Selection Sort", command=lambda: animateSelectionSort(window,canvas,array,sortedArray)) #using lambda allows to pass arguments to function
-    selectionSort.place(x=0,y=200)
-    newGraph = tkinter.Button(window, text="randomize array", command=lambda: drawingGraph(shuffler(array, len(array)),canvas, rectLastX,width))
-    newGraph.place(x=0,y=300)
+def animateSorting(window,canvas):
+    selectionSort = tkinter.Button(canvas, text="Selection Sort", command=lambda: animateSelectionSort(window, canvas, generatedArray))
+    bubbleSort = tkinter.Button(canvas, text="buble sort", command=lambda: animateBubbleSort(window, canvas, generatedArray)) #using lambda allows to pass arguments to function
+    newGraph = tkinter.Button(canvas, text="new graph", command=lambda: drawingGraph(generatedArray, canvas))
+
+    selectionSort.pack()
+    newGraph.pack()
+    bubbleSort.pack()
+
+    slider = tkinter.Scale(canvas,from_=10,to=100,orient='horizontal',command=sliderChanged, variable=currentValue)
+    slider.pack()
     while True:
-        window.mainloop()
+        i = getCurrentValue()
+        if getCurrentValue != i:
+            generatedArray = generateNewArray()
+            drawingGraph(generatedArray,canvas)
+
         window.update()
 
-#****variables used****#
 
-
-lenghtOfArray = 31 # define how long you want your array to be (not the last number)
-startOfArray = 1
 #arr = np.arange(startOfArray,lenghtOfArray,dtype=None) # arr = np.arange(lenghtOfArray+1 if you want to include last number)
-arr = np.random.randint(10, 300, size = 20)
+
 #print(arr, "original")
 #arr = np.delete(arr, findIndexByNumber(arr, 0))                                                                                 #bug, when there is zero in the code it freaks out and runs forever because arrays dont match
-n = len(arr)
-width = 50 #width of the collumn
-rectLastX = 50 # distance from left top edge
-shuffledArray = shuffler(arr, n) # first argument for array and second for array length
+
+ # first argument for array and second for array length
 animation_refresh_seconds = 0.05
-sortedArray = selectionSortForTest(copy(arr))
+
+
 
 animation_window = create_animation_window()
 animation_canvas = create_animation_canvas(animation_window)
-animateSorting(animation_window,animation_canvas,shuffledArray,sortedArray)
+currentValue = tkinter.DoubleVar()
+generatedArray = np.array([])
+generatedArray = generateNewArray()
+animateSorting(animation_window,animation_canvas)
