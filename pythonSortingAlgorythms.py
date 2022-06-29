fc7f9358a79029b354bccb9705d55acb9e408f29
AdomasDauda <107749872+AdomasDauda@users.noreply.github.com>
2022-06-29 14:22:24 +0100

    new gui and colors
    new gui and colors + sorting algorythms + more stuff and fix for quicksort being slow

--- pythonSortingAlgorythms.py
+++ pythonSortingAlgorythms.py
@@ -1,11 +1,15 @@
 from copy import copy
+from logging import PlaceHolder
+from matplotlib.pyplot import draw
 import numpy as np
 import random
 import tkinter
 from tkinter import *
 import time
 import os
-
+import matplotlib
+import winsound
+import threading
 
 def selectionSortForTest(array):
     for index in range(0,array.size):
@@ -32,31 +36,29 @@ def shuffler (arr):
         j = random.randint(0,i+1)
         # Swap arr[i] with the element at random index
         arr[i],arr[j] = arr[j],arr[i]
-    #print(arr, "shuffle")
     return arr
 
 
 def create_animation_window():
     win = tkinter.Tk()
-    win.title("Shuffled")
+    win.title("Python Sorting Algorythms")
+    win.attributes('-fullscreen',True)
+    global length
+    global height
+
+    length = win.winfo_screenwidth()
+    height = win.winfo_screenheight()
+    #geometrya = [length, "x", height]
+    #geometrya = ''.join([str(elem) for elem in geometrya])
+    #win.geometry(geometrya)
     return win
 
 
 def create_animation_canvas(window):
     canvas = tkinter.Canvas(window)
-    canvas.configure(bg="grey")
+    canvas.configure(bg="black")
     canvas.pack(fill="both", expand=True)
-    window.geometry("2500x1000")
     return canvas
-
-
-# find the sum of the array (used it for debugging)
-def sum(array):
-    sum=0
-    for i in array:
-        sum = sum + i
-         
-    return(sum)
 
 
 def flipbool(bool):
@@ -104,15 +106,27 @@ def findIndexByNumber(array, number):
 
 
 def drawingGraph(array,canvas):
-    width = 50
-    rectLastX = 50
+    global drawingCounter
+    start = time.perf_counter()
+    width = 30
+    rectLastX = 20
     canvas.delete("all")
+    indicator = canvas.create_rectangle(0,0,100,100, fill="white", tags="indicator")
+    if sortingInProgress == False:
+        slider.configure(state="active")
+        canvas.delete(indicator)
+    if sortingInProgress == True:
+        slider.configure(state="disabled")
     for i in range(array.size):
         case = array[i]
-        canvas.create_rectangle(rectLastX+width, 0, rectLastX+10, case*4,fill='green')
+        canvas.create_rectangle(rectLastX+width, height-case*4, rectLastX+10, height,fill=matplotlib.colors.to_hex([case/255,case/255,case/255]),tags="graph")
         rectLastX += width
-        canvas.create_text(rectLastX-width/2, case*4,text=case, fill="Black")
+        canvas.create_text(rectLastX-width/2, height-case*4-20,text=case, fill=matplotlib.colors.to_hex([1-case/255,1-case/255,1-case/255]),tags="graph")
         canvas.pack()
+    time.sleep(animation_refresh_seconds)
+    animation_window.update()
+    finish = time.perf_counter()
+    drawingCounter += finish - start
 
 
 def partition(array, low, high):
@@ -129,13 +143,11 @@ def partition(array, low, high):
             # If element smaller than pivot is found
             # swap it with the greater element pointed by i
             i = i + 1
-
             # Swapping element at i with element at j
             (array[i], array[j]) = (array[j], array[i])
         # Swap the pivot element with the greater element specified by i
     (array[i + 1], array[high]) = (array[high], array[i + 1])
     drawingGraph(array,animation_canvas)
-    animation_window.update()
     time.sleep(animation_refresh_seconds)
     
     # Return the position from where partition is done
@@ -145,7 +157,6 @@ def partition(array, low, high):
 # Function to perform quicksort
 def quickSort(array, low, high):
     if low < high:
- 
         # Find pivot element such that
         # element smaller than pivot are on the left
         # element greater than pivot are on the right
@@ -156,15 +167,9 @@ def quickSort(array, low, high):
 
         # Recursive call on the right of pivot
         quickSort(array, pi + 1, high)
-        return array
+    return array
 
 
-def animateQuickSort(window,canvas,array):
-    for j in range(array.size):
-        arraycopy = copy(array)
-        array = quickSort(array,0,len(array)-1)
-        if np.array_equal(array, arraycopy):
-            break
 
 
 def selectionSort(array,index):
@@ -180,52 +185,108 @@ def selectionSort(array,index):
 
 
 def bubbleSort(array):
-        for i in range(array.size-1):
-            if array[i] > array[i+1]:
-                array[i],array[i+1] = array[i+1],array[i]
-        return array
+    for i in range(array.size-1):
+        if array[i] > array[i+1]:
+            array[i],array[i+1] = array[i+1],array[i]
+    return array
 
 
-def animateBubbleSort(window,canvas, array):
+def animateStupidSort(array):
+    global drawingCounter
+    drawingCounter = 0.0
+    start = time.perf_counter()
+    index = 0
+    global sortingInProgress
+    sortingInProgress = True
+    while index < len(array):
+        if index == 0:
+            index = index+1
+        if array[index] >= array[index-1]:
+            index = index+1
+        else:
+            array[index], array[index-1] = array[index-1], array[index]
+            index = index-1
+        drawingGraph(array,animation_canvas)
+    sortingInProgress = False
+    drawingGraph(array,animation_canvas)
+    finish = time.perf_counter()
+    print(f"Finished sorting using Stupidsort in: {(finish - start - drawingCounter)*1000000}ns Animation took:{drawingCounter}s")
+  
+
+
+def animateQuickSort(array, canvas):
+    global drawingCounter
+    drawingCounter = 0.0
+    start = time.perf_counter()
+    global sortingInProgress
+    sortingInProgress = True
+    arraycopy = copy(array)
+    array = quickSort(array,0,array.size-1)
+    sortingInProgress = False   
+    drawingGraph(array,canvas)
+    finish = time.perf_counter()
+    print(f"Finished sorting using Quicksort in: {(finish - start - drawingCounter)*1000000}ns Animation took:{drawingCounter}s")
+
+
+def animateBubbleSort(canvas, array):
+    global drawingCounter
+    drawingCounter = 0.0
+    start = time.perf_counter()
+    global sortingInProgress
+    sortingInProgress = True
     for j in range(array.size):
-        arraycopy = copy(array)
         array = bubbleSort(array)
         drawingGraph(array,canvas)
-        if np.array_equal(array, arraycopy):
-            break
-        window.update()
-        time.sleep(animation_refresh_seconds)
+    sortingInProgress = False
+    drawingGraph(array,canvas)
+    finish = time.perf_counter()
+    print(f"Finished sorting using Bubblesort in: {(finish - start - drawingCounter)*1000000}ns Animation took:{drawingCounter}s")
 
 
-def animateSelectionSort(window,canvas,array):
-    width = 50 #width of the collumn
-    rectLastX = 50 # distance from left top edge
+def animateSelectionSort(canvas,array):
+    global drawingCounter
+    drawingCounter = 0.0
+    start = time.perf_counter()
+    global sortingInProgress
+    sortingInProgress = True
     for i in range(array.size):
         array = selectionSort(array, i)
         drawingGraph(array,canvas)
-        canvas.create_line(2*width+width*i, 0, 2*width+i*width, len(array)*15, width=4) #<---- I want to implement a barrier between sorted and unsorted numbers
-        canvas.create_text(2*width+width*i, len(array)*15,text="After this line everything is sorted",fill="black")
-        window.update()
-        time.sleep(animation_refresh_seconds)
+    sortingInProgress = False
+    drawingGraph(array,canvas)
+    finish = time.perf_counter()
+    print(f"Finished sorting using Selectionsort in: {(finish - start - drawingCounter)*1000000}ns Animation took:{drawingCounter}s")
             
 #***main code***#
 
 def animateSorting(window,canvas):
-    
+    global sortingInProgress
+    sortingInProgress = False
+    global change
+    change = False
+    global drawingCounter
+    drawingCounter = 0.0  
 
-    selectionSort = tkinter.Button(canvas, text="selection Sort", command=lambda: animateSelectionSort(window, canvas, generatedArray))
-    bubbleSort = tkinter.Button(canvas, text="buble sort", command=lambda: animateBubbleSort(window, canvas, generatedArray)) #using lambda allows to pass arguments to function
-    quickSort = tkinter.Button(canvas, text="quick sort", command=lambda: animateQuickSort(window, canvas, generatedArray))
+    exitButton = tkinter.Button(canvas, text="exit", command=animation_window.destroy, activebackground="red",activeforeground="white",bd=0, bg="white")
+    placeholder = tkinter.Button(canvas, text="placeholder", activebackground="blue",activeforeground="white",bd=0, bg="white")
 
+    selectionSort = tkinter.Button(canvas, text="selection Sort", command=lambda: animateSelectionSort(canvas, generatedArray),bd=0)
+    bubbleSort = tkinter.Button(canvas, text="buble sort", command=lambda: animateBubbleSort(canvas, generatedArray),bd=0) #using lambda allows to pass arguments to function
+    quickSort = tkinter.Button(canvas, text="quick sort", command=lambda: animateQuickSort(generatedArray, canvas),bd=0)
+    #stupidSort = tkinter.Button(canvas, text="stupid sort", command=lambda: animateStupidSort(generatedArray))  <<-- its working but very slow animation
+
+    exitButton.place_configure(x=length-29,y=0)
+    placeholder.place_configure(x=length-50,y=20)
+    #stupidSort.pack()
     selectionSort.pack()
     bubbleSort.pack()
     quickSort.pack()
 
-    slider = tkinter.Scale(canvas,from_=20,to=40,orient='horizontal',command=sliderChanged, variable=currentValue)
+    global slider
+    slider = tkinter.Scale(canvas,from_=5,to=60,orient='horizontal',command=sliderChanged, variable=currentValue, label="Array size",bd=0,sliderlength=30)
     slider.pack()
-    generatedArray = np.array([])
-    generatedArray = generateNewArray()
-    global change
+
+
     while True:
         if change == True:
             generatedArray = generateNewArray()
@@ -234,12 +295,15 @@ def animateSorting(window,canvas):
         window.update()
 
 
-animation_refresh_seconds = 0.05
+animation_refresh_seconds = 0
 
 
 animation_window = create_animation_window()
 animation_canvas = create_animation_canvas(animation_window)
-animation_window.protocol('WM_DELETE_WINDOW', animation_window.destroy)
+#animation_window.protocol('WM_DELETE_WINDOW', animation_window.destroy)
 currentValue = tkinter.DoubleVar()
-change = False
+width = 50
+rectLastX = 50
+generatedArray = np.array([])
+generatedArray = generateNewArray()
 animateSorting(animation_window,animation_canvas)
